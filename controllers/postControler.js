18fae6b59578ff138dda600f0bb215a2de909466
Nikolay-Petrov-Ognyanov <nikolaypetrovognyanov25051995@gmail.com>
2023-05-09 18:54:25 +0300

    add get post

--- controllers/postControler.js
+++ controllers/postControler.js
@@ -6,10 +6,11 @@ const postController = require("express").Router()
 postController.get("/", async (req, res) => {
     try {
         const posts = await postService.getAllPosts()
-    
+
         res.status(200).json(posts)
     } catch (error) {
         console.error(error)
+
         res.status(400).json({ message: error.message })
     }
 })
@@ -21,16 +22,27 @@ postController.post("/", async (req, res) => {
         await postService.createPost(req.body.title, req.body.content)
     } catch (error) {
         console.error(error)
+
         res.status(400).json({ message: error.message })
     }
 })
 
-postController.post("/:postId", async (req, res) => {
+postController.get("/:postId", async (req, res) => {
+    const post = await Post.findById(req.params.postId)
+
+    res.status(200).json(post)
+})
+
+postController.post("/:postId/comments", async (req, res) => {
     try {
-        const post = await Post.findByIdAndUpdate(req.params.postId, { $push: { comments: req.body } }, { new: true })
+        const post = await Post.findByIdAndUpdate(
+            req.params.postId, { $push: { comments: req.body } }, { new: true }
+        )
+
         res.status(200).json(post)
     } catch (error) {
         console.error(error)
+
         res.status(400).json({ message: error.message })
     }
 })
